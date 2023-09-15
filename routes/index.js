d89371985d17a4f1732ec336ba0ec1d66b16ec66
yutongz7S2023 <yutongz7@andrew.cmu.edu>
2023-09-15 16:00:34 -0700

    Username and Password Validation

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,27 @@
 let express = require('express');
 let router = express.Router();
+let auth = require('./auth');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index');
 });
 
 /* POST join page. */
-router.post('/join', function(req, res, next) {
+router.post('/join', function (req, res, next) {
   const { username, password } = req.body;
-  const message = `Join Successful: username=${username}, password=${password}`;
-  res.send(message);
+  if (!auth.validateUsername(username)) {
+    res.render('index', {
+      errormsg: "Please provide a different username"
+    })
+  } else if (!auth.validatePassword(password)) {
+    res.render('index', {
+      errormsg: "Please provide a different password"
+    })
+  } else {
+    const message = `Join Successful: username=${username}, password=${password}`;
+    res.send(message);
+  }
 });
 
 module.exports = router;

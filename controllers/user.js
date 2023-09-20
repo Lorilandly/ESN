c35fc230186b0b783d286854132405cd437f7082
Herbie Rand <hrand@andrew.cmu.edu>
2023-09-19 19:56:54 -0700

    Set username to lowercase when calling User Model methods

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,11 @@
 let User = require("../models/user").User;
 
 function create(username, password) {
-	User.create(username, password, "SUPERDUPERADMIN", "DEAD");
+	User.create(username.toLowerCase(), password, "SUPERDUPERADMIN", "DEAD");
 }
 
 async function findByName(name) {
-	return await User.findByName(name);
+	return await User.findByName(name.toLowerCase());
 }
 
 module.exports = { create, findByName };

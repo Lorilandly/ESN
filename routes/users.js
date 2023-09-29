c80a39bbaa0d673ee74a61832e116df1023a03fe
Herbie Rand <hrand@andrew.cmu.edu>
2023-09-29 15:35:35 -0700

    Format code, add CONTRIBUTING Instructions

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,28 @@
-import express from "express";
+import express from 'express';
 let router = express.Router();
 
 import {
-	authenticateUser,
-	create,
-	validateUsernamePassword,
-} from "../controllers/auth.js";
+    authenticateUser,
+    create,
+    validateUsernamePassword,
+} from '../controllers/auth.js';
 
 /* POST new user */
 router.post(
-	"/",
-	await validateUsernamePassword,
-	(_, res, next) => {
-		// Make sure user do not bypass username password checks
-		if (res.locals.data.msg) {
-			res.status(403).send({ message: "You sus" });
-		}
-		next();
-	},
-	await create,
-	authenticateUser,
-	(_, res) => {
-		res.redirect("welcome");
-	}
+    '/',
+    await validateUsernamePassword,
+    (_, res, next) => {
+        // Make sure user do not bypass username password checks
+        if (res.locals.data.msg) {
+            res.status(403).send({ message: 'You sus' });
+        }
+        next();
+    },
+    await create,
+    authenticateUser,
+    (_, res) => {
+        res.redirect('welcome');
+    },
 );
 
 export default router;
--- routes/welcome.js
+++ routes/welcome.js
@@ -1,10 +1,10 @@
-import express from "express";
-import { checkUserAuthenticated } from "../controllers/auth.js";
+import express from 'express';
+import { checkUserAuthenticated } from '../controllers/auth.js';
 let router = express.Router();
 
 /* GET users listing. */
-router.get("/", checkUserAuthenticated, (req, res) => {
-	res.render("welcome");
+router.get('/', checkUserAuthenticated, (req, res) => {
+    res.render('welcome');
 });
 
 export default router;

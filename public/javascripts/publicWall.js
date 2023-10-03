86dcaec8c59b728153934c5cab82211f1a288e8c
Haoan Li <haoanli@andrew.cmu.edu>
2023-10-02 23:58:10 -0700

    Edit comments

--- public/javascripts/publicWall.js
+++ public/javascripts/publicWall.js
@@ -1,18 +1,17 @@
 $(document).ready(() => {
     // Capture form submission event
     $('#messageForm').submit((event) => {
-        event.preventDefault(); // Prevent the default form submission
+        event.preventDefault();
 
         // Get the message body from the input field
         let messageBody = $('#message').val();
 
-        // Make an AJAX request to publicMessages API
+        // Create message by calling API
         $.ajax('/publicMessages', {
             method: 'POST',
-            data: { message: messageBody }, // Pass data to the API
+            data: { message: messageBody },
             dataType: 'json', // Specify the response data type
             success: (response) => {
-                // Handle the API response
                 let resultDiv = $('#result');
                 resultDiv.empty(); // Clear previous results
                 resultDiv.append('<p>API Response:</p>');

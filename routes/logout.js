4cdf57a8e2b92755c1575ef936ae5344ef3799b8
Herbie Rand <hrand@andrew.cmu.edu>
2023-10-04 16:33:56 -0700

    Revert logout PUT to GET due to form

--- routes/logout.js
+++ routes/logout.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { deauthenticateUser } from '../controllers/auth.js';
 let router = express.Router();
 
-router.put('/', deauthenticateUser, (req, res) => {
+router.get('/', deauthenticateUser, (req, res) => {
     return res.redirect('/');
 });
 

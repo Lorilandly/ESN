366ddc694c12ae342d497cc966ba2e847eabdf0c
Judy Zhang <yifanzh7@andrew.cmu.edu>
2023-10-04 15:54:25 -0700

    Modify timestamp display
    Co-authored-by: Herbie Rand hrand@andrew.cmu.edu
    Co-authored-by: Yifan(Judy) Zhang yifanzh7@andrew.cmu.edu

--- models/message.js
+++ models/message.js
@@ -57,6 +57,7 @@ class MessageModel {
         if (queryResponse.rowCount == 0) {
             return null;
         } else {
+            queryResponse.rows.forEach(row => row['time'] = row['time'].toLocaleString());
             return queryResponse.rows;
         }
     }

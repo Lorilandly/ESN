ed5baddf1381b28d9e4d16d6edb9720a863f8ac0
Haoan Li <haoanli@andrew.cmu.edu>
2023-10-09 20:46:42 -0700

    Initial REST endpoint for status
    Co-authored-by: Audrey Zhou <yutongz7@andrew.cmu.edu>

--- routes/users.js
+++ routes/users.js
@@ -35,6 +35,24 @@ router.post(
     },
 );
 
+router.post(
+    '/:id/status',
+    await passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        userId = req.params.id;
+    },
+);
+
+router.get(
+    '/:id/status', (req, res) => {
+        passport.authenticate('jwt', (err, user) => {
+            if (user) return res.status(200).json({status: user.currentStatus});
+            if (err) return res.status(500).json({});
+            return res.status(401).json({});
+        })(req, res);
+    }
+);
+
 router.put(
     '/login',
     passport.authenticate('local', { session: false }),

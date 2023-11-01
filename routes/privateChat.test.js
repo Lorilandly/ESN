367065fa86b921dadd2898cb27191bc96d96c740
Haoan Li <haoanli@andrew.cmu.edu>
2023-11-01 14:45:47 -0700

    Code refractoring

--- routes/privateChat.test.js
+++ routes/privateChat.test.js
@@ -3,12 +3,13 @@ import MockStrategy from 'passport-mock-strategy';
 import request from 'supertest';
 import app from '../app.js';
 
+beforeAll(() => {
+    passport.use('jwt', new MockStrategy());
+});
+
 describe('private chat routes', () => {
     test('get /', async () => {
-        passport.use('jwt', new MockStrategy());
-
         const res = await request(app).get('/privateChat/1');
         expect(res.statusCode).toBe(200);
     });
 });
-
--- routes/status.test.js
+++ routes/status.test.js
@@ -2,12 +2,25 @@ import passport from 'passport';
 import MockStrategy from 'passport-mock-strategy';
 import request from 'supertest';
 import app from '../app.js';
+import DatabaseManager from '../db.js';
+
+beforeAll(() => {
+    passport.use('jwt', new MockStrategy());
+    // do db setups
+    // const dbManager = DatabaseManager.getInstance();
+    // dbManager.configureTestDB();
+    // dbManager.activateTestDB();
+});
 
 describe('status routes', () => {
     test('get /', async () => {
-        passport.use('jwt', new MockStrategy());
-
         const res = await request(app).get('/status');
         expect(res.statusCode).toBe(200);
     });
+});
+
+afterAll(() => {
+    // dismantle db
+    // const dbManager = DatabaseManager.getInstance();
+    // dbManager.deactivateTestDB();
 });

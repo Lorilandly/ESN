5b49875ecc8ff5dd214a79d5b151f55c9cb365cc
yongcheqF2023 <yongcheq@andrew.cmu.edu>
2023-11-07 15:42:03 -0800

    Changed status.js controller syntax
    Co-authored-by: Haoan Li <haoanli@andrew.cmu.edu>

--- controllers/status.js
+++ controllers/status.js
@@ -1,11 +1,11 @@
 import StatusModel from '../models/status.js';
 // get userid，update status by id
 async function updateUserStatus(req, res, next) {
-    await new StatusModel(req.user.id, req.body.status, new Date()).persist();
-    return req.user
-        .updateStatus(req.body.status)
-        .then(() => next())
-        .catch(() => res.sendStatus(500));
+    return new StatusModel(req.user.id, req.body.status, new Date())
+      .persist()
+      .then(() => req.user.updateStatus(req.body.status))
+      .then(() => next())
+      .catch(() => res.sendStatus(500));
 }
 
 

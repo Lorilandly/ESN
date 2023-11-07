53f450e0f230216d29914c7a38c2b212d4236454
Judy Zhang <yifanzh7@andrew.cmu.edu>
2023-11-07 11:24:23 -0800

    Improve unit interfacing for user model

--- controllers/auth.js
+++ controllers/auth.js
@@ -205,15 +205,15 @@ async function create(req, res, next) {
         32,
         'sha256',
     );
-    const user = new UserModel(
-        username.toLowerCase(),
-        passwordHash,
-        salt,
-        'OFFLINE',
-        'UNDEFINED',
-        new Date(Date.now()).toLocaleString(),
-        'SUPERDUPERADMIN',
-    );
+    const user = new UserModel({
+        username: username.toLowerCase(),
+        passwordHash: passwordHash,
+        salt: salt,
+        loginStatus: 'OFFLINE',
+        status: 'UNDEFINED',
+        statusTime: new Date(Date.now()).toLocaleString(),
+        privilege: 'SUPERDUPERADMIN'
+    });
     await user.persist();
     return next();
 }

fbcd909636d0bb27c98997b0dc8e25c0e5c2b6e7
Haoan Li <haoanli@andrew.cmu.edu>
2023-11-20 15:37:11 -0800

    Profile frontend initialize

---
+++ public/javascripts/profile.js
@@ -0,0 +1,17 @@
+$(document).ready(() => {
+    $('#status-form').submit((event) => {
+        event.preventDefault();
+        $.ajax({
+            url: '/users/status',
+            method: 'PUT',
+            data: { status: $('#status').find(':selected').val() },
+            dataType: 'json',
+            success: () => {
+                location.reload();
+            },
+            error: (err) => {
+                console.error('status update somehow error', err);
+            },
+        });
+    });
+});

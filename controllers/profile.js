f00e552cb4796c1d2811cbea6bb6b3a39ddf4d97
Haoan Li <haoanli@andrew.cmu.edu>
2023-11-22 17:14:06 -0800

    Send help backend

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,3 +1,4 @@
+import nodemailer from 'nodemailer';
 import ProfileModel from '../models/profile.js';
 
 /**
@@ -38,4 +39,40 @@ async function removeUserProfile(id, key) {
     return new ProfileModel(id, key, null).removeProfileEntry();
 }
 
-export { getUserProfile, updateUserProfile, addUserProfile, removeUserProfile };
+async function sendHelp(user) {
+    const emctEmail = await getUserProfile(user.id, true).then((res) =>
+        res.find((entry) => entry.key === '_emct_email'),
+    );
+    if (!emctEmail || !emctEmail.val) {
+        throw new Error('Emergency contact email address not set.');
+    }
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'esn8306@gmail.com',
+            pass: 'peirzddetpadmvji',
+        },
+    });
+
+    const mailOptions = {
+        from: 'esn8306@gmail.com',
+        to: emctEmail.val,
+        subject: 'Request for help',
+        text: `${user.username} needs your help!`,
+    };
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            throw err;
+        } else {
+            return 'Email sent: ' + info.response;
+        }
+    });
+}
+
+export {
+    getUserProfile,
+    updateUserProfile,
+    addUserProfile,
+    removeUserProfile,
+    sendHelp,
+};

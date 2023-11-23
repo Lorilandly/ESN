f480a6602d57865645041500a8fc04dde04924e2
Haoan Li <haoanli@andrew.cmu.edu>
2023-11-22 18:34:08 -0800

    Add view profile page

--- routes/profile.js
+++ routes/profile.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import passport from 'passport';
+import { getUserProfile } from '../controllers/profile.js';
 
 const router = express.Router();
 
@@ -7,6 +8,10 @@ router.use(passport.authenticate('jwt', { session: false }));
 
 router.get('/', (req, res) => {
     return res.render('profile');
+});
+
+router.get('/:id', async (req, res) => {
+    return res.render('profile-mini', { profile: await getUserProfile(req.params.id, false) });
 });
 
 export default router;
